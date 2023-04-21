0f75d157714acc03667e6d952ddfc7c3c2f53558
Vladislav Sergeev <Tionlierite@gmail.com>
2023-04-21 18:29:01 +0300

    initial commit

---
+++ src/app/Components/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+const Home = () => {
+	return (
+		<>
+			<h1>Home</h1>
+			<Button variant='contained'>Some Button</Button>
+		</>
+	)
+}
+
+export default Home
---
+++ src/app/Components/navBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+	return (
+		<ul>
+			<li style={{ display: 'inline', margin: 5 }}>
+				<Link to='/'>Домой</Link>
+			</li>
+			<li style={{ display: 'inline', margin: 5 }}>
+				<Link to='/algorithms'>Алгоритмы</Link>
+			</li>
+			<li style={{ display: 'inline', margin: 5 }}>
+				<Link to='/dictionary'>Справочник</Link>
+			</li>
+			<li style={{ display: 'inline', margin: 5 }}>
+				<Link to='/calculator'>Калькулятор</Link>
+			</li>
+			<li style={{ display: 'inline', margin: 5 }}>
+				<Link to='/tables'>Таблицы</Link>
+			</li>
+		</ul>
+	)
+}
+
+export default NavBar
