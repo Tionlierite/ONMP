8557d4469f82d0595d89f3546d57c4be905ac01a
Tionlierite <Tionlierite@gmail.com>
2023-06-06 17:22:34 +0300

    feat: added Basic styles
    specially for demonstration

--- src/pages/feature-pages/createCard/statusLocalis.jsx
+++ src/pages/feature-pages/createCard/statusLocalis.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { Button } from '@mui/material'
 
 const StatusLocalis = () => {
 	const statusLocalis = useSelector(state => state.tables.tablesResult)
 	return (
-		<>
-			<h2>Status localis</h2>
-			<Link to='/tables'>
-				<button>Перейти в таблицы</button>
-			</Link>
+		<div className='status-localis'>
+			<div className='status-localis-container'>
+				<h2>Status localis</h2>
+				<Link to='/tables'>
+					<Button variant='outlined'>Перейти в таблицы</Button>
+				</Link>
+			</div>
 			<h3>{statusLocalis}</h3>
-		</>
+		</div>
 	)
 }
 
